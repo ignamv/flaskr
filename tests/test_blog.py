8e3d366a71de98652f44747c17901e00ac3b52b4
Ignacio Martinez Vazquez <ignamv@gmail.com>
2022-01-02 10:42:20 +0100

    Move "like" tests to separate file

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,4 +1,3 @@
-import re
 import pytest
 from flaskr.db import get_db
 
@@ -93,48 +92,3 @@ def test_singlepost(client):
     response = client.get("/2").data
     assert b"test title" not in response
     assert b"test2" in response
-
-
-def test_like_missing_post(client, auth):
-    auth.login()
-    assert client.post("/2000/like", data={"like": "1"}).status_code == 404
-
-
-def test_like_redirects(client, auth):
-    auth.login()
-    response = client.post("/1/like", data={"like": "1"})
-    assert response.headers["Location"] == "http://localhost/"
-    referer = "http://localhost/the_referer"
-    response = client.post("/2/like", data={"like": "1"}, headers={"Referer": referer})
-    assert response.headers["Location"] == referer
-
-
-def test_like(client, auth):
-    def like(user, post, status):
-        auth.login(user)
-        client.post(f"/{post}/like", data={"like": "1" if status else "0"})
-        auth.logout()
-
-    def assert_likes(user, likes1, likes2):
-        auth.login(user)
-        likes1 = b"Unlike" if likes1 else b"Like"
-        likes2 = b"Unlike" if likes2 else b"Like"
-        assert likes1 in client.get("/1").data
-        assert likes2 in client.get("/2").data
-        assert re.search(likes2 + b".*" + likes1, client.get("/").data, flags=re.DOTALL)
-        auth.logout()
-
-    user1 = "test"
-    user2 = "other"
-    like(user2, 1, True)
-    assert_likes(user2, True, False)
-    assert_likes(user1, False, False)
-    like(user1, 1, True)
-    assert_likes(user1, True, False)
-    like(user1, 2, True)
-    assert_likes(user1, True, True)
-    like(user1, 1, False)
-    assert_likes(user1, False, True)
-    like(user1, 2, False)
-    assert_likes(user1, False, False)
-    assert_likes(user2, True, False)
