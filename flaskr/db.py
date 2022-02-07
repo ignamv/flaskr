e668fd64b22f637ddc0d920d2ebc17b176c8e220
Ignacio Martinez Vazquez <ignamv@gmail.com>
2022-02-07 12:46:11 +0100

    Make database return timezone-aware DateTime objects

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 import click
 from flask import current_app, g
@@ -9,6 +10,10 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(current_app.config['DATABASE'],
                                detect_types=sqlite3.PARSE_DECLTYPES)
+        sqlite3.register_converter(
+            'TIMESTAMP',
+            lambda s: datetime.datetime.fromisoformat(s.decode() + '+00:00')
+        )
         g.db.row_factory = sqlite3.Row
     return g.db
 
