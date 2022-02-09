95d557afdaff8a886b7d213ce1dad7481a5c5f82
Ignacio Martinez Vazquez <ignamv@gmail.com>
2022-02-09 06:36:00 +0100

    Refactor auth functions into separate module

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,10 +1,12 @@
 import functools
+from sqlite3 import IntegrityError
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
+from flaskr.auth_db import (
+    register_user, check_login_credentials, WrongPasswordException, load_user)
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -14,7 +16,6 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
 
         if not username:
@@ -24,12 +25,8 @@ def register():
 
         if error is None:
             try:
-                db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
+                register_user(username, password)
+            except IntegrityError:
                 error = f'User {username} is already registered'
             else:
                 return redirect(url_for('auth.login'))
@@ -44,13 +41,12 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
-        user = db.execute('SELECT * FROM user WHERE username = ?',
-                          (username,)).fetchone()
-        if user is None:
+        try:
+            user = check_login_credentials(username, password)
+        except KeyError:
             error = 'Incorrect username'
-        elif not check_password_hash(user['password'], password):
+        except WrongPasswordException:
             error = 'Incorrect password'
 
         if error is None:
@@ -70,8 +66,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute('SELECT * FROM user WHERE id = ?',
-                                  (user_id,)).fetchone()
+        g.user = load_user(user_id)
 
 
 def get_user_id():
